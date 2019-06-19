b8af3898550398b0cbf8ead8949df645c3265d2a
[Dorijan] <[dorijan11@gmail.com]>
2019-06-19 15:10:48 +0200

    connected react to redux

---
+++ src/store/reducer.js
@@ -0,0 +1,29 @@
+const initialState = {
+  counter: 0
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1
+      };
+    case "ADDITION":
+      return {
+        counter: state.counter + action.val
+      };
+
+    case "SUBTRACTION":
+      return {
+        counter: state.counter - action.val
+      };
+  }
+
+  return state;
+};
+
+export default reducer;
